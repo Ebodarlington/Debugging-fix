3a39b786d50ef3a4594a9998e82ab7328518d2f8
Ebodarlington <ycm.darlington@gmail.com>
2025-10-15 20:33:43 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "TerraformWebInstance"
+  }
+}
